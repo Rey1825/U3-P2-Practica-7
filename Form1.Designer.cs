45bf6f9bd8651c7e0b6d6ee1b7c71740677a4bc5
reyda <reyda@LAPTOP-ALH488S6>
2023-11-27 00:55:00 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace U3_P2_Practica_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(345, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa escribe del 1 al 5";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Rockwell", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 31;
+            listBox1.Location = new Point(66, 71);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(236, 283);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Sienna;
+            button1.Font = new Font("Stencil", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(85, 381);
+            button1.Name = "button1";
+            button1.Size = new Size(202, 41);
+            button1.TabIndex = 2;
+            button1.Text = "Listar numeros";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Tan;
+            ClientSize = new Size(368, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "1 a 5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
